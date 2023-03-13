20611a8df4a399d5c3fbc76dba7b832a8ba810bf
Hendrik Pienaar <hendrikpienaar@Hendriks-iMac.local>
2023-03-13 16:45:44 +0200

    Before adding router

--- quizApp/src/App.vue
+++ quizApp/src/App.vue
@@ -1,24 +1,64 @@
 <script setup>
+  import q from '@/data/quizes.json';
+  import { ref, watch } from 'vue';
+  import BaseCard from '@/components/BaseCard.vue';
+
+  const quizes = ref(q);
+  const search = ref("");
+  watch(search, ()=> {
+    quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+  });
 
 </script>
 
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input v-model="search" type="text" placeholder="Search...">
+    </header>
+    <div class="options-container">
+      <BaseCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img 
+          :src="quiz.img" 
+          alt="">
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.lenght }}</p>
+        </div>
+      </div> -->
+    </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+.container {
+  max-width: 300px;
+  margin:  0 auto;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
 }
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+.options-container {
+  display: flex;
+  /* flex-wrap: wrap; */
+  margin-top: 40px;
+  width: fit-content;
+}
+
 </style>
